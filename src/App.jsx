91f237845b12522e3c9dca0e15e17a4fbf724836
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-03-08 16:02:49 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Carousel from './components/Carousel/Carousel'
-import Header from './components/Header'
-import Analysis from './sections/Analysis'
-import Hero from './sections/Hero'
-import Negotiation from './sections/Negotiation'
-import Platforms from './sections/Platforms'
-import Webinarios from './sections/Webinarios'
-import News from './sections/News'
-import About from './sections/About'
-import Faq from './sections/Faq'
-import Footer from './components/Footer'
+import Carousel from "./components/Carousel/Carousel";
+import Header from "./components/Header";
+import Analysis from "./sections/Analysis";
+import Hero from "./sections/Hero";
+import Negotiation from "./sections/Negotiation";
+import Platforms from "./sections/Platforms";
+import Webinarios from "./sections/Webinarios";
+import News from "./sections/News";
+import About from "./sections/About";
+import Faq from "./sections/Faq";
+import Footer from "./components/Footer";
+import StaticTable from "./components/StaticTable";
 
 function App() {
   return (
@@ -23,9 +24,10 @@ function App() {
       <News />
       <About />
       <Faq />
+      <StaticTable />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
