2d275df714cf6a070757452e15e2394a9bcf747c
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-03-08 16:14:51 -0300

    Tabela Fot-Markets

---
+++ public/index.js
@@ -0,0 +1,4 @@
+import arrowDown from "./arrow-down.svg";
+import arrowUp from "./arrow-up.svg";
+
+export { arrowDown, arrowUp };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,165 +1,819 @@
 export const analysis1 = [
-  'Mercado aberto por mais de 20 horas/diária',
-  'Ordens Executadas em menos de 1 segundo',
-  'Taxa de Execução 100%, sem rejeição de ordens',
-  'Execução de Mercado em tempo real'
+  "Mercado aberto por mais de 20 horas/diária",
+  "Ordens Executadas em menos de 1 segundo",
+  "Taxa de Execução 100%, sem rejeição de ordens",
+  "Execução de Mercado em tempo real",
 ];
 
 export const analysis2 = [
-  'Diariamente atualização das perspectivas de mercado',
-  'Indicadores de Tendência',
-  'Calendário Econômico e suas influências no mercado',
-  'Calculadora Forex'
-]
+  "Diariamente atualização das perspectivas de mercado",
+  "Indicadores de Tendência",
+  "Calendário Econômico e suas influências no mercado",
+  "Calculadora Forex",
+];
 
 export const tutorials1 = [
   {
     id: 1,
-    imgUrl: '/sala-ao-vivo.svg',
-    text: 'Salas ao vivo'
+    imgUrl: "/sala-ao-vivo.svg",
+    text: "Salas ao vivo",
   },
   {
     id: 2,
-    imgUrl: '/youtube.svg',
-    text: 'Vídeos com conteúdo educacional'
+    imgUrl: "/youtube.svg",
+    text: "Vídeos com conteúdo educacional",
   },
   {
     id: 3,
-    imgUrl: '/television.svg',
-    text: 'Webnários'
+    imgUrl: "/television.svg",
+    text: "Webnários",
   },
-]
+];
 
 export const tutorials2 = [
   {
     id: 4,
-    imgUrl: '/book.svg',
-    text: 'Tutoriais'
+    imgUrl: "/book.svg",
+    text: "Tutoriais",
   },
   {
     id: 5,
-    imgUrl: 'light-bulb.svg',
-    text: 'Dicas de filmes, livros e vídeos'
+    imgUrl: "light-bulb.svg",
+    text: "Dicas de filmes, livros e vídeos",
   },
   {
     id: 6,
-    imgUrl: '/graphics.svg',
-    text: 'Calculadora forex'
-  }
-]
+    imgUrl: "/graphics.svg",
+    text: "Calculadora forex",
+  },
+];
 
 export const negotiations = [
-  'Plataforma Metrader com acesso gratuito e ilimitado',
-  'Possibilidade de transacionar lotes micros ou conta stand',
-  'Atualizações Gratuitas e Diárias sobre o mercado e a análise técnica dos ativos',
-  'Apoio em inglês, espanhol e português',
-  'Atendimento suporte 24/7',
-  'Gestor Pessoal de Conta'
-]
+  "Plataforma Metrader com acesso gratuito e ilimitado",
+  "Possibilidade de transacionar lotes micros ou conta stand",
+  "Atualizações Gratuitas e Diárias sobre o mercado e a análise técnica dos ativos",
+  "Apoio em inglês, espanhol e português",
+  "Atendimento suporte 24/7",
+  "Gestor Pessoal de Conta",
+];
 
 export const webinarios = [
-  'Webinários diários e gratuitos, apresentados por nossos',
-  'especialistas, feito para você, que quer aprender de forma rápida e',
-  'interativa, ou para quem já opera, mas quer acompanhar as',
-  'análises dos movimentos diários do mercado. Nossos especialistas',
-  'realizam análises técnicas e fundamentalistas e demonstram o que',
-  'estão visualizando no mercado e as tendências para os períodos.'
-]
+  "Webinários diários e gratuitos, apresentados por nossos",
+  "especialistas, feito para você, que quer aprender de forma rápida e",
+  "interativa, ou para quem já opera, mas quer acompanhar as",
+  "análises dos movimentos diários do mercado. Nossos especialistas",
+  "realizam análises técnicas e fundamentalistas e demonstram o que",
+  "estão visualizando no mercado e as tendências para os períodos.",
+];
 
 export const about = [
   {
     id: 1,
-    imgUrl: '/support.svg',
-    text: 'Suporte Humanizado 12/6'
+    imgUrl: "/support.svg",
+    text: "Suporte Humanizado 12/6",
   },
   {
     id: 2,
-    imgUrl: '/account-management.svg',
-    text: 'Gestores de Contas'
+    imgUrl: "/account-management.svg",
+    text: "Gestores de Contas",
   },
   {
     id: 3,
-    imgUrl: '/chats.svg',
-    text: 'Chat ao vivo'
+    imgUrl: "/chats.svg",
+    text: "Chat ao vivo",
   },
   {
     id: 4,
-    imgUrl: '/group-chat.svg',
-    text: 'Webinários'
+    imgUrl: "/group-chat.svg",
+    text: "Webinários",
   },
   {
     id: 5,
-    imgUrl: '/globe-live.svg',
-    text: 'Salas ao vivo'
+    imgUrl: "/globe-live.svg",
+    text: "Salas ao vivo",
   },
 ];
 
 export const faq = [
   {
     id: 1,
-    title: 'O que é uma conta forex?',
-    label: 'É uma conta de negociação que funciona de forma semelhante a uma conta bancária, mas com a diferença de que é principalmente emitida com o objetivo de negociar moedas.'
+    title: "O que é uma conta forex?",
+    label:
+      "É uma conta de negociação que funciona de forma semelhante a uma conta bancária, mas com a diferença de que é principalmente emitida com o objetivo de negociar moedas.",
   },
   {
     id: 2,
-    title: 'Posso ter várias contas de negociação?',
-    label: 'Sim, pode, qualquer cliente pode ter até 10 contas na FOT.'
+    title: "Posso ter várias contas de negociação?",
+    label: "Sim, pode, qualquer cliente pode ter até 10 contas na FOT.",
   },
   {
     id: 3,
-    title: 'Como posso gerir minhas contas de negociação?',
-    label: 'Você mesmo pode gerir, é fácil e intuitivo. Mas conta com nosso suporte 24/7, nosso chat online por 12h e ainda conta com seu gestor de contas.'
+    title: "Como posso gerir minhas contas de negociação?",
+    label:
+      "Você mesmo pode gerir, é fácil e intuitivo. Mas conta com nosso suporte 24/7, nosso chat online por 12h e ainda conta com seu gestor de contas.",
   },
   {
     id: 4,
-    title: 'Como posso aprender a operar?',
-    label: 'Pode escolher um curso, estudar, mas também conta com nossas salas ao vivo, webinários e em breve teremos cursos e eventos presenciais.'
-  }
-]
+    title: "Como posso aprender a operar?",
+    label:
+      "Pode escolher um curso, estudar, mas também conta com nossas salas ao vivo, webinários e em breve teremos cursos e eventos presenciais.",
+  },
+];
 
 export const getInTouch1 = [
   {
     id: 1,
-    imgUrl: '/phone-icon.svg',
-    label: '0800 123456789'
+    imgUrl: "/phone-icon.svg",
+    label: "0800 123456789",
   },
   {
     id: 2,
-    imgUrl: '/@-icon.svg',
-    label: 'fotmarkets@fotmarkets.com'
-  }
-]
+    imgUrl: "/@-icon.svg",
+    label: "fotmarkets@fotmarkets.com",
+  },
+];
 
 export const getInTouch2 = [
   {
     id: 1,
-    imgUrl: '/chat-icon.svg',
-    label: 'Acesse o chat'
+    imgUrl: "/chat-icon.svg",
+    label: "Acesse o chat",
   },
   {
     id: 2,
-    imgUrl: '/headphone-icon.svg',
-    label: 'Callcenter'
-  }
-]
+    imgUrl: "/headphone-icon.svg",
+    label: "Callcenter",
+  },
+];
 
 export const footerContent1 = [
-  'Documentos legais',
-  'Termos e condições',
-  'Bônus de negociação e depósitos',
-  'Programa de bônus'
-]
+  "Documentos legais",
+  "Termos e condições",
+  "Bônus de negociação e depósitos",
+  "Programa de bônus",
+];
 
 export const footerContent2 = [
-  'Programa de fidelidade',
-  'Termos e condições VPS',
-  'Prevenção de branqueamento de capitais',
-  'Política de execução de ordens'
-]
+  "Programa de fidelidade",
+  "Termos e condições VPS",
+  "Prevenção de branqueamento de capitais",
+  "Política de execução de ordens",
+];
 
 export const footerContent3 = [
-  'Divulgação de risco para instrumentos financeiros',
-  'Polítia de conflito de interesse',
-  'Procedimentos para tratamento de reclamações',
-  'Programa de afiliado'
-]
+  "Divulgação de risco para instrumentos financeiros",
+  "Polítia de conflito de interesse",
+  "Procedimentos para tratamento de reclamações",
+  "Programa de afiliado",
+];
+
+export const standardAcount = [
+  {
+    key: "1",
+    coin: "AUDCAD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00028",
+    avarageSpread: "0.0003",
+    longSwap: "-6.29",
+    shortSwap: "-1.24",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "2",
+    coin: "AUDCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00028",
+    avarageSpread: "0.0003",
+    longSwap: "2.1",
+    shortSwap: "-7.9",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+  {
+    key: "3",
+    coin: "AUDJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.032",
+    avarageSpread: "0.033",
+    longSwap: "5.73",
+    shortSwap: "-14.77",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "4",
+    coin: "AUDNZD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00037",
+    avarageSpread: "0.0004",
+    longSwap: "-13.08",
+    shortSwap: "-0.18",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "5",
+    coin: "AUDUSD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00016",
+    avarageSpread: "0.00018",
+    longSwap: "-4.24",
+    shortSwap: "0.26",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "6",
+    coin: "CADCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00035",
+    avarageSpread: "0.00037",
+    longSwap: "4.48",
+    shortSwap: "-10.23",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "7",
+    coin: "CADJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.031",
+    avarageSpread: "0.034",
+    longSwap: "9.78",
+    shortSwap: "-18.22",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "8",
+    coin: "CHFJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.028",
+    avarageSpread: "0.033",
+    longSwap: "-2.06",
+    shortSwap: "-11.86",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "9",
+    coin: "CHFSGD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00282",
+    avarageSpread: "0.0031",
+    longSwap: "-26.81",
+    shortSwap: "2.29",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "10",
+    coin: "EURAUD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00027",
+    avarageSpread: "0.0003",
+    longSwap: "-11.14",
+    shortSwap: "-2.54",
+    value: "100000",
+    level: "0",
+    option: 10,
+  },
+
+  {
+    key: "11",
+    coin: "EURCAD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00029",
+    avarageSpread: "0.0003",
+    longSwap: "-13.8",
+    shortSwap: "2",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "12",
+    coin: "EURCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00023",
+    avarageSpread: "0.00027",
+    longSwap: "0.62",
+    shortSwap: "-9.48",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "13",
+    coin: "EURDKK****",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00385",
+    avarageSpread: "0.0039",
+    longSwap: "-64.98",
+    shortSwap: "-79.98",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "14",
+    coin: "EURGBP",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00018",
+    avarageSpread: "0.0002",
+    longSwap: "-6.32",
+    shortSwap: "1.28",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "15",
+    coin: "EURHKD****",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0041",
+    avarageSpread: "0.0045",
+    longSwap: "-66.37",
+    shortSwap: "-63.27",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "16",
+    coin: "EURHUF",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.334",
+    avarageSpread: "0.59",
+    longSwap: "-203.49",
+    shortSwap: "132.31",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "17",
+    coin: "EURJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.021",
+    avarageSpread: "0.023",
+    longSwap: "7.66",
+    shortSwap: "-16.54",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "18",
+    coin: "EURNOK",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.012",
+    avarageSpread: "0.01455",
+    longSwap: "-105.82",
+    shortSwap: "-76.72",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "19",
+    coin: "EURNZD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00039",
+    avarageSpread: "0.0004",
+    longSwap: "-24.13",
+    shortSwap: "5.37",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "20",
+    coin: "EURPLN",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0045",
+    avarageSpread: "0.0065",
+    longSwap: "-100",
+    shortSwap: "19.7",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "21",
+    coin: "EURSEK",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00765",
+    avarageSpread: "0.0099",
+    longSwap: "-149.71",
+    shortSwap: "-124.71",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "22",
+    coin: "EURSGD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00161",
+    avarageSpread: "0.0017",
+    longSwap: "-18,93",
+    shortSwap: "-4.83",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "23",
+    coin: "EURTRY****",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0191",
+    avarageSpread: "0.043",
+    longSwap: "-1099.09",
+    shortSwap: "9.11",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "24",
+    coin: "EURUSD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00016",
+    avarageSpread: "0.00017",
+    longSwap: "-9,39",
+    shortSwap: "3,41",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "25",
+    coin: "EURZAR",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.01734",
+    avarageSpread: "0.0241",
+    longSwap: "-418,45",
+    shortSwap: "108.05",
+    value: "100000",
+    level: "0",
+    option: 20,
+  },
+
+  {
+    key: "26",
+    coin: "GBPAUD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00036",
+    avarageSpread: "0.00038",
+    longSwap: "-4,9",
+    shortSwap: "-10,5",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "27",
+    coin: "GBPCAD",
+    minimumFluctuation: "0.000471",
+    lowSpread: "0.00047",
+    avarageSpread: "0.0005",
+    longSwap: "-8.58",
+    shortSwap: "-4.78",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "28",
+    coin: "GBPCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00037",
+    avarageSpread: "0.00046",
+    longSwap: "5.68",
+    shortSwap: "-15.62",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "29",
+    coin: "GBPDKK****",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0111",
+    avarageSpread: "0.012",
+    longSwap: "-36.6",
+    shortSwap: "-126.4",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "30",
+    coin: "GBPJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.032",
+    avarageSpread: "0.036",
+    longSwap: "15.74",
+    shortSwap: "-25.66",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "31",
+    coin: "GBPNOK",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0183",
+    avarageSpread: "0.0216",
+    longSwap: "-59.86",
+    shortSwap: "-134.86",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "32",
+    coin: "GBPNZD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00062",
+    avarageSpread: "0.0007",
+    longSwap: "-18.86",
+    shortSwap: "-2.06",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "33",
+    coin: "GBPSEK",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00945",
+    avarageSpread: "0.0134",
+    longSwap: "-55.45",
+    shortSwap: "-135.55",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "34",
+    coin: "GBPSGD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00276",
+    avarageSpread: "0.0031",
+    longSwap: "-14.35",
+    shortSwap: "-12.35",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "35",
+    coin: "GBPUSD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00019",
+    avarageSpread: "0.00021",
+    longSwap: "-5.37",
+    shortSwap: "-1.17",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "36",
+    coin: "NZDCAD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00038",
+    avarageSpread: "0.00041",
+    longSwap: "-2.15",
+    shortSwap: "-7.35",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "37",
+    coin: "NZDCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00038",
+    avarageSpread: "0.0004",
+    longSwap: "4.42",
+    shortSwap: "-11.58",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "38",
+    coin: "NZDJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.025",
+    avarageSpread: "0.03",
+    longSwap: "8.94",
+    shortSwap: "-19.76",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "39",
+    coin: "NZDSGD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00265",
+    avarageSpread: "0.0028",
+    longSwap: "-5.27",
+    shortSwap: "-11.47",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "40",
+    coin: "NZDUSD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00025",
+    avarageSpread: "0.00027",
+    longSwap: "-1.19",
+    shortSwap: "-4.39",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "41",
+    coin: "SGDJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.195",
+    avarageSpread: "0.2",
+    longSwap: "-7.59",
+    shortSwap: "-7.73",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "42",
+    coin: "USDCAD",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0002",
+    avarageSpread: "0.00022",
+    longSwap: "-3.23",
+    shortSwap: "-4.73",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "43",
+    coin: "USDCHF",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00019",
+    avarageSpread: "0.00021",
+    longSwap: "7.72",
+    shortSwap: "-13.38",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "44",
+    coin: "USDCNH****",
+    minimumFluctuation: "0.0001",
+    lowSpread: "0.0034",
+    avarageSpread: "0.0036",
+    longSwap: "-5.26",
+    shortSwap: "-15.26",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "45",
+    coin: "USDDKK****",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.00332",
+    avarageSpread: "0.0036",
+    longSwap: "-17.16",
+    shortSwap: "-116.86",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "46",
+    coin: "USDHKD****",
+    minimumFluctuation: "0.0001",
+    lowSpread: "0.0026",
+    avarageSpread: "0.0028",
+    longSwap: "-1.49",
+    shortSwap: "-13.01",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "47",
+    coin: "USDHUF",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.305",
+    avarageSpread: "0.51",
+    longSwap: "-159",
+    shortSwap: "96.1",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "48",
+    coin: "USDJPY",
+    minimumFluctuation: "0.001",
+    lowSpread: "0.015",
+    avarageSpread: "0.016",
+    longSwap: "15.16",
+    shortSwap: "-24.14",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "49",
+    coin: "USDMXN",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.0197",
+    avarageSpread: "0.0239",
+    longSwap: "-698.58",
+    shortSwap: "21.42",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+
+  {
+    key: "50",
+    coin: "USDNOK",
+    minimumFluctuation: "0.00001",
+    lowSpread: "0.014",
+    avarageSpread: "0.017",
+    longSwap: "-33.68",
+    shortSwap: "-134.88",
+    value: "100000",
+    level: "0",
+    option: 30,
+  },
+];
