a5f84d63f0265aec480043e2fba0abe7964187e1
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-03-10 14:50:04 -0300

    carousel update

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,27 @@ import Faq from "./sections/Faq";
 import Footer from "./components/Footer";
 import StaticTable from "./components/StaticTable";
 
+const OPTIONS = {};
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
 function App() {
-  return (
-    <div className="overflow-hidden">
-      <Header />
-      <Hero />
-      <Platforms />
-      <Webinarios />
-      <Analysis />
-      <Carousel />
-      <Negotiation />
-      <News />
-      <About />
-      <Faq />
-      <StaticTable />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="overflow-hidden">
+			<Header />
+			<Hero />
+			<Platforms />
+			<Webinarios />
+			<Analysis />
+			<Carousel slides={SLIDES} options={OPTIONS} />
+			<Negotiation />
+			<News />
+			<About />
+			<Faq />
+			<StaticTable />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
